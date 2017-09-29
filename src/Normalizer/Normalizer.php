55a6f952e64eaa53f29d6c31dab76692f6f73542
Rob Treacy <robert.treacy@thesalegroup.co.uk>
2017-09-29 13:37:25 +0100

    Fixed undefined variable

--- src/Normalizer/Normalizer.php
+++ src/Normalizer/Normalizer.php
@@ -88,7 +88,7 @@ class Normalizer
     private function getTransformer($type): TransformerInterface
     {
         if (!array_key_exists($type, $this->transformers)) {
-            throw new Exception\UnknownTransformerException(sprintf('No transformer for type "%s" exists.', $propertyType));
+            throw new Exception\UnknownTransformerException(sprintf('No transformer for type "%s" exists.', $type));
         }
 
         return $this->transformers[$type];
